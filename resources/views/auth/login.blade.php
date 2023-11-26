ce4cd6cdb919e7447e6177edcfc24db471c072d9
Piyawit <onepiece25492006@gmail.com>
2023-11-26 22:37:18 +0700

    add dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,46 +18,45 @@
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-5">
 					<h2 class="heading-section">Login</h2>
-				</div>
+			    </div>
 			</div>
 			<div class="row justify-content-center">
 				<div class="col-md-6 col-lg-4">
 					<div class="login-wrap p-0">
-		      	<h3 class="mb-4 text-center">Have an account?</h3>
+		      			<h3 class="mb-4 text-center">Have an account?</h3>
 
-		    <form class="signin-form" method="POST" action="{{ route('login') }}">
-        		@csrf
+		        			<form class="signin-form" method="POST" action="{{ route('login') }}"> 
+        						@csrf
 
-		      		<div class="form-group">
-		      			<input type="text" name="username" class="form-control" placeholder="Username" required>
+								<div class="form-group">
+									<input type="text" name="username" class="form-control" placeholder="Username" required>
+								</div>
+
+								<div class="form-group">
+									<input id="password" type="password" name="password" class="form-control" placeholder="Password" required>
+									<span toggle="#password-field" toggle-password"></span>
+								</div>
+
+								<div class="form-group">
+									<button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+								</div>
+
+								<div class="form-group d-md-flex">
+									<div class="w-50 text-md-left">
+										<a href="{{ route('password.request') }}" style="color: #fff">Forgot Password</a>
+									</div>
+
+									<div class="w-50 text-md-right">
+										<a href="{{ route('register') }}" style="color: #fff">Sing up now</a>
+									</div>
+								</div>
+							</form>
+								<p class="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
+								<div class="social d-flex text-center">
+									<a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span> Facebook</a>
+									<a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span> Twitter</a>
+								</div>
 		      		</div>
-
-	            <div class="form-group">
-	              <input id="password" type="password" name="password" class="form-control" placeholder="Password" required>
-	              <span toggle="#password-field" toggle-password"></span>
-	            </div>
-
-            <div class="form-group">
-	            	<button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
-	            </div>
-
-	            <div class="form-group d-md-flex">
-	            	
-								<div class="w-50 text-md-left">
-									<a href="{{ route('password.request') }}" style="color: #fff">Forgot Password</a>
-								</div>
-
-								<div class="w-50 text-md-right">
-									<a href="{{ route('register') }}" style="color: #fff">Sing up now</a>
-								</div>
-	            </div>
-	        </form>
-	          <p class="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
-	          <div class="social d-flex text-center">
-	          	<a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span> Facebook</a>
-	          	<a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span> Twitter</a>
-	          </div>
-		      </div>
 				</div>
 			</div>
 		</div>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,75 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Login 10</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="{{ asset('authen/css/style.css') }}">
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+	</head>
+	<body class="img js-fullheight" style="background-image: url('{{ asset('authen/images/project.jpg') }}');">
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Reset Password</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-4">
+					<div class="login-wrap p-0">
+		      	       <h3 class="mb-4 text-center"></h3>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                        <form class="signin-form" method="POST" action="{{ route('password.store') }}">
+                            @csrf
+                            <!-- Password Reset Token -->
+                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
+                            <div class="form-group">
+                                <input type="email" name="email" class="form-control" placeholder="Email" value="{{ $request->email }}" required>
+                            </div>
+                            <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                            <div class="form-group">
+                                <input id="password" type="password" name="password" class="form-control" placeholder="Password" required>
+                                <span toggle="#password-field" toggle-password"></span>
+                            </div>
+                            <x-input-error :messages="$errors->get('password')" class="mt-2 text-danger" />
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+                            <div class="form-group">
+                                <input id="password" type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password" required>
+                                <span toggle="#password-field" toggle-password"></span>
+                            </div>
+                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 text-danger" />
+
+                            <div class="form-group">
+                                <button type="submit" class="form-control btn btn-primary submit px-3">Sign Up</button>
+                            </div>
+
+                            <div class="form-group d-md-flex">
+                                <div class="w-50 text-md-left">
+                                    <a href="{{ route('login') }}" style="color: #fff">Sing in now</a>
+                                </div>
+                            </div>
+                                    
+                        </form>
+	                        
+		            </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="{{ asset('authen/js/jquery.min.js') }}"></script>
+  <script src="{{ asset('authen/js/popper.js') }}"></script>
+  <script src="{{ asset('authen/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('authen/js/main.js') }}"></script>
+
+	</body>
+</html>
+
